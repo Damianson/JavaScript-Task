89518d363cf4146ff1c33a12d78cc3981044d5e8
Damianson <damiansonuchegbu2017@gmail.com>
2020-03-22 10:54:23 +0100

    Initial commit

---
+++ jstask.js
@@ -0,0 +1,12 @@
+var name = "Uchegbu Damianson-Wisdom Onyekachi";
+var courses = ["HTML", "CSS", "JavaScript", "Flutter", "Python"];
+var x = courses.toString();
+var i;
+
+console.log(name);
+console.log(x);
+
+for(i = 1; i <= 200;) {
+    console.log(i);
+    i = i + 2
+}
